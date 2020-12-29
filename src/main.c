6dcf856802ce80a6410eb3b67ef1e63784149b74
Benjamin <bgiraudrenard56@gmail.com>
2020-12-29 01:49:28 +0100

    Programme fonctionnel

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #define PI 3.14159265359
 
 void draw_circle(int x_centre, int y_centre, int rayon, int color) {
-	//bresenham
+	//Cercle avec Bresenham
     int x, y, m;
     x = 0;
     y = rayon;
@@ -30,30 +30,64 @@ void draw_circle(int x_centre, int y_centre, int rayon, int color) {
 
 int main(void)
 {
-	dclear(C_BLACK);
+	int nbPoint = 100;
+	int rayon = 90;
 
-	int nbPoint = 10;
-	int rayon = 40;
+	float facteur = 2;
 
 	int width = DWIDTH/2;
 	int height = DHEIGHT/2;
 
-	int facteur = 2;
-	draw_circle(width,height,rayon, C_WHITE);
+	int boucle = 1;
 
-	for(int i = 0; i < nbPoint; i++) {
-		double angle = 2 * PI * i / nbPoint;
-		int x = rayon * cos(angle);
-		int y = rayon * sin(angle);
-		dpixel(x+width, y+height, C_RED);
+	char diapo = 0;
+	char mode = 0;
+	while(boucle) {
+		dclear(C_BLACK);
+		draw_circle(width,height,rayon, C_WHITE);
+		for(int i = 0; i < nbPoint; i++) {
+			double angle = 2 * PI * i / nbPoint;
+			//affiche les points du cercle 
+			/*int x = rayon * cos(angle);
+			int y = rayon * sin(angle);
+			draw_circle(x+width, y+height, 3, C_RED);*/
+			double link = i*facteur;
+			while(link >= nbPoint) link-=nbPoint;
+			double anglelink = 2 * PI * link / nbPoint;
+			dline(rayon * cos(angle)+width, rayon * sin(angle)+height, rayon * cos(anglelink)+width, rayon * sin(anglelink)+height, C_WHITE);
+		}
+		if(!mode) dprint(1,1,C_WHITE,"Table : %.1j", (int)(facteur*10));
+		else dprint(1,1,C_WHITE,"nbPoint : %d",nbPoint);
+		dupdate();
+
+		clearevents();
+		if(keydown(KEY_SHIFT)) {
+			diapo = 1;
+		}
+		if(keydown(KEY_ALPHA)) {
+			diapo = 0;
+		}
+		if(keydown(KEY_F1)) {
+			mode = 0;
+		}
+		if(keydown(KEY_F2)) {
+			mode = 1;
+		}
+		if(keydown(KEY_EXIT)) {
+				boucle = 0;
+		}
+		if(!diapo) {
+			if(keydown(KEY_RIGHT)) {
+				if(!mode) facteur+=0.1;
+				else nbPoint++;
+			} else if(keydown(KEY_LEFT)) {
+				if(!mode) facteur-=0.1;
+				else if(nbPoint > 10) nbPoint--;
+			}
+		} else {
+			facteur+=0.1;
+		}
 	}
 
-	for(int i = 0; i < nbPoint; i++) {
-		int link = (i*facteur)%nbPoint;
-		dprint(10, i*15, C_RED, "%d : %d", i, link);
-	}
-	
-	dupdate();
-	getkey();
 	return 1;
 }
